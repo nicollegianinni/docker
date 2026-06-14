ebff3637eef165c2f75d445f6c9aa388e27c6699
CassianoPL <gianinni.nicolle@gmail.com>
2026-06-14 16:08:04 -0300

    config dockerfile

---
+++ world-hello/src/main/java/world_hello/WorldHelloApplication.java
@@ -0,0 +1,13 @@
+package world_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldHelloApplication.class, args);
+	}
+
+}
---
+++ world-hello/src/test/java/world_hello/WorldHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package world_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
